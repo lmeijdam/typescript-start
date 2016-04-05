4c146425c058a737e09364d710fbbb30c9a54f9f
Lars Meijdam <lmeijdam90@gmail.com>
2016-04-05 21:06:52 +0200

    updated main.ts with a full implementation of the interfaces and inheritance

--- 04/main.ts
+++ 04/main.ts
@@ -10,9 +10,11 @@ interface IRunnable {
     run(): string;
 }
 
-class AnimalBase {
-    constructor(name: string) {
+abstract class AnimalBase {
+    name: string;
 
+    constructor(name: string) {
+        this.name = name;
     }
 }
 
@@ -22,7 +24,7 @@ class Dog extends AnimalBase implements IRunnable {
     }
 
     run() {
-        return "i'm Running";
+        return "I will keep on running";
     }
 }
 
@@ -32,7 +34,7 @@ class Bird extends AnimalBase implements IFlyable {
     }
 
     fly() {
-        return "i'm Flying";
+        return "I'd love to fly";
     }
 }
 
@@ -42,6 +44,57 @@ class Fish extends AnimalBase implements ISwimmable {
     }
 
     swim() {
-        return "i'm Swimming";
+        return "I love swimming";
     }
+}
+
+//var animal = new AnimalBase(); // Results in error because AnimalBase is abstract
+var Lassie = new Dog("Lassie");
+var Flipper = new Fish("Flipper");
+var Zazu = new Bird("Zazu");
+
+// Lassie.fly() // Error
+// Zazu.run() // error
+
+var parentContainer = document.createElement("div");
+
+// Dog
+var dogParagraph = document.createElement("div");
+dogParagraph.innerHTML = createHtml(Lassie.name, Lassie.run(), "http://cx.aos.ask.com/question/aq/700px-394px/kind-dog-lassie_870333c6a15c5d1b.jpg");
+
+// Bird
+var birdParagraph = document.createElement("div");
+birdParagraph.innerHTML = createHtml(Zazu.name, Zazu.fly(), "http://vignette4.wikia.nocookie.net/thelionkingstimonandpumbaa/images/3/38/Zazu_onrock.jpg/revision/latest?cb=20121111174415");
+
+// Fish
+var fishParagraph = document.createElement("div");
+fishParagraph.innerHTML = createHtml(Flipper.name, Flipper.swim(), "https://ryanlbrooks.files.wordpress.com/2011/06/dsc1024.jpg");
+
+parentContainer.appendChild(dogParagraph);
+parentContainer.appendChild(birdParagraph);
+parentContainer.appendChild(fishParagraph);
+
+document.body.appendChild(parentContainer);
+
+// Template
+function createHtml(name: string, action: string, imageUrl?: string): string {
+    return `
+    ${getLabel(name, imageUrl)}
+    ${getQuote(action)} 
+    `;
+}
+
+function getLabel(name: string, imageUrl?: string): string {
+    if (!imageUrl)
+        return `<p><b>${name}</b></p>`;
+    return `<p>
+    <img src="${imageUrl}" height="100"/>
+    <p><b>${name}</b></p>
+        </p>`;
+
+}
+
+function getQuote(action: string): string {
+    return `<blockquote><p>${action}</p></blockquote>`;
+
 }
