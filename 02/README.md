62397a800e0a70e90b1a89ec80d463ed7255703d
Don <donheuvelman@outlook.com>
2016-04-13 20:33:33 +0200

    CH02, finished TS

--- 02/README.md
+++ 02/README.md
@@ -1,12 +1,12 @@
 ## 02 - Types, Classes & Access Modifiers
 
-Nu we de basis beginselen van TypeScript hebben ontdekt kunnen we eens verder gaan kijken. In dit hoofdstuk kijken we naar Types binnen TypeScript.
+Nu we de basis beginselen van TypeScript hebben ontdekt kunnen we eens verder gaan kijken. 
+In dit hoofdstuk kijken we naar classes, types en access modifiers zodat we object georienteerd kunnen gaan proberen.
+
 Twee types hebben we al gezien, teksten (string) en getallen (Number).
 
+1. Maak op een locatie naar keuze een folder aan en noem deze 'HelloTypeScript'. Vanuit deze folder beginnen we het 'project'.
 
-Iets met auto's doen
-- Types, Classes & Access Modifiers.
-- Enums,
-
+2. Maak in de root van deze folder een nieuw bestaand aan en noem deze 'main.js'. (De .ts extensie gebruiken we later) 
 
 TODO: Stap 16 uit hoofdstuk 1 halen en dat in dit hoofdstuk behandelen.
--- 02/main.ts
+++ 02/main.ts
@@ -1,9 +1,10 @@
 enum Colors {
-    'Apple Green',
-    'Diamond Silver',
-    'Candy Red',
-    'Chocolate Brown',
-    'Stone Cole Black'
+    Green,
+    Gray,
+    Red,
+    White,
+    Brown,
+    Black
 }
 
 class Car {
@@ -18,7 +19,7 @@ class Car {
         this.doors = doors;
         this.color = color;
 
-        console.log("Car created: " + brand + " " + type + " color: " + color.toString() + "(doors: " +  doors + " )" );
+        console.log("Car created: " + brand + " " + type + ". Color: " + Colors[color] + ". Doors: " +  doors);
     }
 }
 
@@ -33,16 +34,67 @@ class Garage{
         
     closeDoor(){
         this.doorOpen = false;
+        this.showDoorStatus();
     }
     
     openDoor(){
         this.doorOpen = true;
+        this.showDoorStatus();
+    }
+    
+    showDoorStatus(){
+        
+        if(this.doorOpen) {
+            console.log("Door status: Open");
+        }
+        else {
+            console.log("Door status: Closed");
+        }
     }
     
     addCar(car: Car){
-        var currentAmountOfCars = this.cars.length;
-        this.cars[currentAmountOfCars + 1] = car;
+
+        if(this.doorOpen) {
+            var currentAmountOfCars = this.cars.length;
+            this.cars[currentAmountOfCars] = car;
+            console.log("Car added to garage: " + car.brand + " " + car.type);
+        }
+        else {
+            console.log("Couldn't add car. Reason: The garage door is closed. Please open the door first.");
+        }
     }    
 }
 
-// todo: HTML show, if the door is open. If open, show the amount of cars in the garage and the brands and colors of these cars.
+
+class Main{
+    
+    public garage: Garage;
+    
+    private car1: Car;
+    private car2: Car;
+    private car3: Car;  
+    
+    constructor(){       
+        this.garage = new Garage();
+        
+        this.car1 = new Car("Nissan", "GTR R35", 2, Colors.Black);
+        this.car2 = new Car("Alfa Romeo", "Giulia", 4, Colors.Red);
+        this.car3 = new Car("BMW", "i8", 2, Colors.White);
+    }
+    
+    run(){       
+        this.garage.addCar(this.car1);
+        this.garage.openDoor();
+        this.garage.addCar(this.car2);
+        this.garage.addCar(this.car3);
+        this.garage.closeDoor();
+    }
+}
+
+
+var main = new Main();
+main.run();
+
+console.log("Garage amount of cars: " + main.garage.cars.length);
+
+console.log("Brand of Car1: " + main.car1.brand); // Throws TypeScript Exception: Property 'car1' is private and only accessible within class 'main'.
